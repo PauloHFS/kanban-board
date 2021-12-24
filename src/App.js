12592939d7c94f0e49558e953958059b2d70652a
Paulo Hernane <paulohernane10@gmail.com>
2021-12-24 18:11:29 -0300

    Adicionando o conteudo desejado na tela

--- src/App.js
+++ src/App.js
@@ -1,74 +1,112 @@
-import "./App.css";
-import Main from "./components/Main/Main";
-import Column from "./components/Column/Column";
-import Card from "./components/Card/Card";
+import './App.css';
+import Main from './components/Main/Main';
+import Column from './components/Column/Column';
+import Card from './components/Card/Card';
 
-import todoIcon from "./assets/column/todo.svg";
-import male from "./assets/avatars/Male.png";
+import todoIcon from './assets/column/todo.svg';
+import inprogressIcon from './assets/column/inprogress.svg';
+import reviewIcon from './assets/column/review.svg';
+import doneIcon from './assets/column/done.svg';
+
+import maleAvatar from './assets/avatars/Male.png';
+import femaleAvatar from './assets/avatars/Female.png';
 
 function App() {
   return (
     <div>
       <Main title="Kanban">
         <Column
-          icon={<img src={todoIcon} />}
+          icon={<img alt="" src={todoIcon} />}
           title="To Do"
           backgroundColor="blue"
         >
-          <Card label="teste" avatar={<img src={male} />}>
-            Teste
+          <Card
+            label="Label"
+            avatar={<img alt="femaleAvatar" src={femaleAvatar} />}
+          >
+            Christmas Banners
           </Card>
-          <Card label="teste" avatar={<img src={male} />}>
-            Teste 2
-          </Card>
-          <Card label="teste" avatar={<img src={male} />}>
-            Teste 3
+          <Card
+            label="Label"
+            avatar={<img alt="femaleAvatar" src={femaleAvatar} />}
+          >
+            Redo Portfolio
           </Card>
         </Column>
         <Column
-          icon={<img src={todoIcon} />}
-          title="To Do"
+          icon={<img alt="" src={inprogressIcon} />}
+          title="In Progress"
           backgroundColor="orange"
         >
-          <Card label="teste" labelColor="orange" avatar={<img src={male} />}>
-            Teste
+          <Card
+            label="Always"
+            labelColor="orange"
+            avatar={<img alt="maleAvatar" src={maleAvatar} />}
+          >
+            Coffee Break
           </Card>
-          <Card label="teste" labelColor="orange" avatar={<img src={male} />}>
-            Teste 2
-          </Card>
-          <Card label="teste" labelColor="orange" avatar={<img src={male} />}>
-            Teste 3
+          <Card
+            label="Webflow"
+            labelColor="orange"
+            avatar={<img alt="maleAvatar" src={maleAvatar} />}
+          >
+            Updating Portfolio
           </Card>
         </Column>
         <Column
-          icon={<img src={todoIcon} />}
-          title="To Do"
+          icon={<img alt="" src={reviewIcon} />}
+          title="Review"
           backgroundColor="purple"
         >
-          <Card label="teste" labelColor="pink" avatar={<img src={male} />}>
-            Teste
+          <Card
+            label="Release"
+            labelColor="pink"
+            avatar={<img alt="femaleAvatar" src={femaleAvatar} />}
+          >
+            Release to Figma Community
           </Card>
-          <Card label="teste" labelColor="pink" avatar={<img src={male} />}>
-            Teste 2
+          <Card
+            label="Feedback"
+            labelColor="pink"
+            avatar={<img alt="femaleAvatar" src={femaleAvatar} />}
+          >
+            User Feedback
           </Card>
-          <Card label="teste" labelColor="pink" avatar={<img src={male} />}>
-            Teste 3
+          <Card
+            label="Sourcing"
+            labelColor="pink"
+            avatar={<img alt="femaleAvatar" src={femaleAvatar} />}
+          >
+            Background images from{' '}
+            <a href="https://www.humaaans.com/">humaaans.com</a>
           </Card>
         </Column>
         <Column
-          icon={<img src={todoIcon} />}
-          title="To Do"
+          icon={<img alt="" src={doneIcon} />}
+          title="Done"
           backgroundColor="green"
           textColor="black"
         >
-          <Card label="teste" labelColor="green" avatar={<img src={male} />}>
-            Teste
+          <Card
+            label="UI"
+            labelColor="green"
+            avatar={<img alt="maleAvatar" src={maleAvatar} />}
+          >
+            Style Guide
           </Card>
-          <Card label="teste" labelColor="green" avatar={<img src={male} />}>
-            Teste 2
+          <Card
+            label="UI"
+            labelColor="green"
+            avatar={<img alt="maleAvatar" src={maleAvatar} />}
+          >
+            Component Library
           </Card>
-          <Card label="teste" labelColor="green" avatar={<img src={male} />}>
-            Teste 3
+          <Card
+            label="UI"
+            labelColor="green"
+            avatar={<img alt="maleAvatar" src={maleAvatar} />}
+          >
+            Sticker Components
           </Card>
         </Column>
       </Main>
